5fa5ce01dd1767515583d6f75bcf11189c9407b8
Michael Friis <friism@gmail.com>
2016-09-25 18:09:13 -0400

    final fixes

--- src/MusicStore/Startup.cs
+++ src/MusicStore/Startup.cs
@@ -44,9 +44,9 @@ namespace MusicStore
             }
             else
             {
-                System.Console.WriteLine("Using Connectionstring: {0}", Configuration["ConnectionString"]);
+                System.Console.WriteLine("Using Connectionstring: {0}", Configuration["Data:DefaultConnection:ConnectionString"]);
                 services.AddDbContext<MusicStoreContext>(options =>
-                    options.UseSqlServer(Configuration["ConnectionString"]));
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
             }
 
             // Add Identity services to the services container
